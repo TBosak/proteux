db9eb16a454928b676acf96d201ae278d1e942a2
Tim Barani <timb63701@gmail.com>
2023-05-17 22:16:49 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, signal } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import * as Papa from 'papaparse';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit {
   title = 'app';
+  site = new FormControl('');
+  response = signal([] as any[]);
+
+  ngOnInit(): void {
+    this.site.valueChanges.subscribe((value) => {
+      if (value){
+        // axios.get(value).then((res) => {
+        // });
+      }
+    });
+  }
+
+  process(event: any){
+    const target= event.target as HTMLInputElement;
+    const file: File = (target.files as FileList)[0];
+    file.text().then((text) => {
+      console.log(Papa.parse(text, {header:true}).data);
+      this.response.set(Papa.parse(text, {header:true}).data);
+    });
+  }
+
+  keys(obj: any){
+    return Object.keys(obj);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
